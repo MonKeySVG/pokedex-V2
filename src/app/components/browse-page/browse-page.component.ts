a244ec5e081077b3b38a0a8e4b788ec4904f0dbc
MonKeySVG <play95100@gmail.com>
2025-02-06 14:26:41 +0100

    Fonctionnement du router

---
+++ src/app/components/browse-page/browse-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-page',
+  imports: [],
+  templateUrl: './browse-page.component.html',
+  styleUrl: './browse-page.component.css'
+})
+export class BrowsePageComponent {
+
+}
