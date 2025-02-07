8e0a1840c06b2d588bbd226e45de012b66d9bb42
MonKeySVG <play95100@gmail.com>
2025-02-07 16:11:41 +0100

    Affichage de l'image et des statistiques lorsque la carte est flipped

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,12 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Pokemon} from '../../models/pokemon.model';
-import {NgClass, NgIf} from '@angular/common';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-card',
   imports: [
     NgIf,
-    NgClass
+    NgClass,
+    NgForOf
   ],
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.css'
@@ -14,6 +15,9 @@ import {NgClass, NgIf} from '@angular/common';
 export class PokemonCardComponent {
   @Input() pokemon!: Pokemon;
   isFlipped: boolean = false;
+
+  statNames: string[] = ['HP', 'ATK', 'DEF', 'ATK SP', 'DEF SP', 'SPD'];
+
 
   flipCard(): void {
     this.isFlipped = !this.isFlipped;
