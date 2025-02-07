8c6fa972caa979c1cfbdb38c5be99c28ea935466
MonKeySVG <play95100@gmail.com>
2025-02-07 11:01:12 +0100

    Création des pokemon cards

--- src/app/components/pokedex/pokedex.component.ts
+++ src/app/components/pokedex/pokedex.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import {PokedexService} from '../../services/pokedex.service';
 import {Pokemon} from '../../models/pokemon.model';
+import {PokemonCardComponent} from '../pokemon-card/pokemon-card.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-pokedex',
-  imports: [],
+  imports: [
+    PokemonCardComponent,
+    NgForOf
+  ],
   templateUrl: './pokedex.component.html',
   styleUrl: './pokedex.component.css'
 })
---
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+import {Pokemon} from '../../models/pokemon.model';
+import {NgClass, NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-pokemon-card',
+  imports: [
+    NgIf,
+    NgClass
+  ],
+  templateUrl: './pokemon-card.component.html',
+  styleUrl: './pokemon-card.component.css'
+})
+export class PokemonCardComponent {
+  @Input() pokemon!: Pokemon;
+
+}
