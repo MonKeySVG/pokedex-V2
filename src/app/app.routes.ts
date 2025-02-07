c1c09beacd9e1e204a2ba6643c2b1ed02f4c607d
MonKeySVG <play95100@gmail.com>
2025-02-07 13:58:18 +0100

    Refonte du header.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
-import {SearchPageComponent} from './components/search-page/search-page.component';
 import {BrowsePageComponent} from './components/browse-page/browse-page.component';
 import {AboutPageComponent} from './components/about-page/about-page.component';
 
 export const routes: Routes = [
-  { path: 'searchPage', component: SearchPageComponent },
   { path: 'browsePage', component: BrowsePageComponent },
   { path: 'aboutPage', component: AboutPageComponent },
-  { path: '', redirectTo: '/searchPage', pathMatch: 'full' }
+  { path: '', redirectTo: '/browsePage', pathMatch: 'full' }
 ];
