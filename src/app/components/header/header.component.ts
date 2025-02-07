5f42cf7d90f71d620f6054bf1bdb7f2258dbf67a
MonKeySVG <play95100@gmail.com>
2025-02-07 15:01:33 +0100

    Création de l'apparence de la search-bar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {SearchBarComponent} from '../search-bar/search-bar.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    SearchBarComponent,
+    NgIf
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isSearchBarOpen = false;
 
+  openSearchBar() {
+    this.isSearchBarOpen = true;
+  }
+
+  closeSearchBar() {
+    this.isSearchBarOpen = false;
+  }
 }
