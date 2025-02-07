1aa1543da165707557e38bd2fe30527a42f9e01d
MonKeySVG <play95100@gmail.com>
2025-02-07 10:10:57 +0100

    Création de l'observable pokemons dans le pokedex service

--- src/app/components/pokedex/pokedex.component.ts
+++ src/app/components/pokedex/pokedex.component.ts
@@ -14,36 +14,12 @@ export class PokedexComponent {
   constructor(private pokedexService: PokedexService) {}
 
   ngOnInit() {
-    this.pokedexService.getAllPokemons().subscribe({
-      next: data => {
-        this.pokemons = data.map(pokemonData => {
-          const id = pokemonData.id;
-          const name = pokemonData.name;
-          const types = pokemonData.types.map((type: any) => type.type.name);
-          const generation = this.getGeneration(id);
-          const spriteUrl = pokemonData.sprites.front_default;
-          const spriteArtworkUrl = pokemonData.sprites.other['official-artwork'].front_default;
-          const spriteShowdownUrl = pokemonData.sprites.other.showdown.front_default;
-          const height = pokemonData.height / 10;
-          const weight = pokemonData.weight;
-          const stats = pokemonData.stats.map((stat: any) => stat.base_stat);
-          return new Pokemon(id, name, types, generation, spriteUrl, spriteArtworkUrl, spriteShowdownUrl, height, weight, stats);
-        })
-        console.log(this.pokemons);
-      },
-      error: error => {
-        console.error('Error fetching Pokémon data', error);
-      }
+    this.pokedexService.pokemons$.subscribe(data => {
+      this.pokemons = data;
+      console.log(this.pokemons);
     });
+    this.pokedexService.getAllPokemons();
   }
 
-  getGeneration(pokemonId: number): number {
-    if (pokemonId <= 151) return 1;
-    if (pokemonId <= 251) return 2;
-    if (pokemonId <= 386) return 3;
-    if (pokemonId <= 493) return 4;
-    if (pokemonId <= 649) return 5;
-    if (pokemonId <= 721) return 6;
-    return 7;
-  }
+
 }
--- src/app/services/pokedex.service.ts
+++ src/app/services/pokedex.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {forkJoin, Observable} from 'rxjs';
+import {BehaviorSubject, forkJoin, Observable, of, tap} from 'rxjs';
+import {Pokemon} from '../models/pokemon.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,9 @@ import {forkJoin, Observable} from 'rxjs';
 export class PokedexService {
 
   private apiUrl = 'https://pokeapi.co/api/v2/pokemon';
+  private pokemonsSubject = new BehaviorSubject<Pokemon[]>([]);
+  pokemons$ = this.pokemonsSubject.asObservable();
+
 
 
   constructor(private http: HttpClient) {}
@@ -20,9 +24,40 @@ export class PokedexService {
     return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
 
-  getAllPokemons(): Observable<any[]> {
+  getAllPokemons(): void {
+    if (this.pokemonsSubject.getValue().length > 0) {
+      return;
+    }
+
     const pokemonIds = Array.from({ length: 721 }, (_, i) => i + 1);
     const requests = pokemonIds.map(id => this.getPokemonById(id));
-    return forkJoin(requests);
+    forkJoin(requests).pipe(
+      tap(data => {
+        const pokemons = data.map(pokemonData => {
+          const id = pokemonData.id;
+          const name = pokemonData.name;
+          const types = pokemonData.types.map((type: any) => type.type.name);
+          const generation = this.getGeneration(id);
+          const spriteUrl = pokemonData.sprites.front_default;
+          const spriteArtworkUrl = pokemonData.sprites.other['official-artwork'].front_default;
+          const spriteShowdownUrl = pokemonData.sprites.other.showdown.front_default;
+          const height = pokemonData.height / 10;
+          const weight = pokemonData.weight;
+          const stats = pokemonData.stats.map((stat: any) => stat.base_stat);
+          return new Pokemon(id, name, types, generation, spriteUrl, spriteArtworkUrl, spriteShowdownUrl, height, weight, stats);
+        });
+        this.pokemonsSubject.next(pokemons);
+      })
+    ).subscribe();
+  }
+
+  private getGeneration(pokemonId: number): number {
+    if (pokemonId <= 151) return 1;
+    if (pokemonId <= 251) return 2;
+    if (pokemonId <= 386) return 3;
+    if (pokemonId <= 493) return 4;
+    if (pokemonId <= 649) return 5;
+    if (pokemonId <= 721) return 6;
+    return 7;
   }
 }
