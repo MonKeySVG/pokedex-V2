58f6f4e4f5a306f9c43bf01d7bdfed02b81d7e8e
MonKeySVG <play95100@gmail.com>
2025-02-10 15:05:44 +0100

    Implémentation du fonctionnement des propositions de recherche

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,15 +1,41 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {Pokemon} from '../../models/pokemon.model';
+import {PokedexService} from '../../services/pokedex.service';
+import {NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf,
+    FormsModule
+  ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
   @Output() close = new EventEmitter<void>();
+  searchTerm: string = '';
+  filteredPokemons: Pokemon[] = [];
+
+  constructor(private pokedexService: PokedexService) {}
+
 
   closeSearchBar() {
     this.close.emit();
+  }
+
+  onSearchChange() {
+    console.log(this.searchTerm);
+    if (this.searchTerm.length > 0) {
+      this.pokedexService.pokemons$.subscribe(pokemons => {
+        this.filteredPokemons = pokemons.filter(pokemon =>
+          pokemon.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+        );
+      });
+    } else {
+      this.filteredPokemons = [];
+    }
   }
 }
