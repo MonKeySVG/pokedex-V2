29f5a1d9c8d84005df15ce9ec86c73621d5a1f52
MonKeySVG <play95100@gmail.com>
2025-02-10 14:23:21 +0100

    Page détails d'un pokemon fonctionnelle

--- src/app/components/pokemon-details-page/pokemon-details-page.component.ts
+++ src/app/components/pokemon-details-page/pokemon-details-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Pokemon} from '../../models/pokemon.model';
+import {ActivatedRoute} from '@angular/router';
+import {PokedexService} from '../../services/pokedex.service';
 
 @Component({
   selector: 'app-pokemon-details-page',
@@ -7,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './pokemon-details-page.component.css'
 })
 export class PokemonDetailsPageComponent {
+  pokemon!: Pokemon;
 
+  constructor(
+    private route: ActivatedRoute,
+    private pokedexService: PokedexService
+  ) {}
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    this.pokedexService.pokemons$.subscribe(pokemons => {
+      this.pokemon = pokemons.find(p => p.id === id)!;
+    });
+  }
 }
