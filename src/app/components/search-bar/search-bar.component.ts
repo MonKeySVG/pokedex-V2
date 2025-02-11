76df2f5c5010faddb866d1adfdd09c3bbe439ea8
MonKeySVG <play95100@gmail.com>
2025-02-11 09:53:55 +0100

    Ajout de la possibilité de naviguer parmis les résultats de recherche avec les flèches du clavier

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, HostListener, Output} from '@angular/core';
 import {Pokemon} from '../../models/pokemon.model';
 import {PokedexService} from '../../services/pokedex.service';
 import {NgClass, NgForOf, NgIf} from '@angular/common';
@@ -19,6 +19,8 @@ export class SearchBarComponent {
   @Output() close = new EventEmitter<void>();
   searchTerm: string = '';
   filteredPokemons: Pokemon[] = [];
+  selectedIndex: number = -1;
+
 
   constructor(private pokedexService: PokedexService) {}
 
@@ -34,9 +36,31 @@ export class SearchBarComponent {
         this.filteredPokemons = pokemons.filter(pokemon =>
           pokemon.name.toLowerCase().includes(this.searchTerm.toLowerCase())
         );
+        this.selectedIndex = this.filteredPokemons.length > 0 ? 0 : -1;
       });
     } else {
       this.filteredPokemons = [];
+      this.selectedIndex = -1;
+
     }
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      this.selectedIndex = (this.selectedIndex + 1) % this.filteredPokemons.length;
+    } else if (event.key === 'ArrowUp') {
+      event.preventDefault();
+      this.selectedIndex = (this.selectedIndex - 1 + this.filteredPokemons.length) % this.filteredPokemons.length;
+    } else if (event.key === 'Enter' && this.selectedIndex >= 0) {
+      event.preventDefault();
+      this.selectPokemon(this.filteredPokemons[this.selectedIndex]);
+    }
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    // Handle the selection of a Pokemon (e.g., navigate to the Pokemon details page)
+    console.log('Selected Pokemon:', pokemon);
   }
 }
