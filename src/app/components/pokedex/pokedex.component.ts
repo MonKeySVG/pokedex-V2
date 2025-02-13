331fde255f984fc1edeb4d6cc114a71104cbdc08
MonKeySVG <play95100@gmail.com>
2025-02-13 10:49:15 +0100

    Ajout d'une animation d'apparition pour la page pokedex

--- src/app/components/pokedex/pokedex.component.ts
+++ src/app/components/pokedex/pokedex.component.ts
@@ -3,12 +3,14 @@ import {PokedexService} from '../../services/pokedex.service';
 import {Pokemon} from '../../models/pokemon.model';
 import {PokemonCardComponent} from '../pokemon-card/pokemon-card.component';
 import {NgForOf} from '@angular/common';
+import {AnimateOnScrollDirective} from '../../directives/animate-on-scroll.directive';
 
 @Component({
   selector: 'app-pokedex',
   imports: [
     PokemonCardComponent,
-    NgForOf
+    NgForOf,
+    AnimateOnScrollDirective
   ],
   templateUrl: './pokedex.component.html',
   styleUrl: './pokedex.component.css'
---
+++ src/app/directives/animate-on-scroll.directive.ts
@@ -0,0 +1,24 @@
+import {AfterViewInit, Directive, ElementRef, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appAnimateOnScroll]'
+})
+export class AnimateOnScrollDirective implements AfterViewInit{
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.renderer.addClass(this.el.nativeElement, 'animate');
+        } else {
+          this.renderer.removeClass(this.el.nativeElement, 'animate');
+        }
+      });
+    });
+
+    observer.observe(this.el.nativeElement);
+  }
+
+}
