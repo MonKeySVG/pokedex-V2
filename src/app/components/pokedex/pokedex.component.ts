40b074d19090ca26531f5a28b6d05f56a57a069c
MonKeySVG <play95100@gmail.com>
2025-02-13 11:18:15 +0100

    Ajout d'un chargement

--- src/app/components/pokedex/pokedex.component.ts
+++ src/app/components/pokedex/pokedex.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 import {PokedexService} from '../../services/pokedex.service';
 import {Pokemon} from '../../models/pokemon.model';
 import {PokemonCardComponent} from '../pokemon-card/pokemon-card.component';
-import {NgForOf} from '@angular/common';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
 import {AnimateOnScrollDirective} from '../../directives/animate-on-scroll.directive';
 
 @Component({
@@ -10,23 +10,45 @@ import {AnimateOnScrollDirective} from '../../directives/animate-on-scroll.direc
   imports: [
     PokemonCardComponent,
     NgForOf,
-    AnimateOnScrollDirective
+    AnimateOnScrollDirective,
+    NgIf,
+    NgClass
   ],
   templateUrl: './pokedex.component.html',
   styleUrl: './pokedex.component.css'
 })
 export class PokedexComponent {
-  pokemons: Pokemon[] = []
+  pokemons: Pokemon[] = [];
+  isLoading = true;
+  private imagesLoaded = 0;
 
-  constructor(private pokedexService: PokedexService) {}
+  constructor(private pokedexService: PokedexService, private renderer: Renderer2) {}
 
   ngOnInit() {
     this.pokedexService.pokemons$.subscribe(data => {
       this.pokemons = data;
-      console.log(this.pokemons);
+      // console.log(this.pokemons);
     });
     this.pokedexService.getAllPokemons();
   }
 
+  ngAfterViewInit() {
+    this.checkImagesLoaded();
+  }
+
+  checkImagesLoaded() {
+    const images = document.querySelectorAll('img');
+    images.forEach(img => {
+      this.renderer.listen(img, 'load', () => this.onImageLoad());
+    });
+  }
+
+  onImageLoad() {
+    this.imagesLoaded++;
+    console.log(this.imagesLoaded);
+    if (this.imagesLoaded === this.pokemons.length) {
+      this.isLoading = false;
+    }
+  }
 
 }
