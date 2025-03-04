87e085c2f4aa55ea85c7b555a62e27af65d47123
Réda <37266352+MonKeySVG@users.noreply.github.com>
2025-03-04 11:14:17 +0100

    css auto fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component, HostListener } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { NgIf } from '@angular/common';
 import { PokedexService } from '../../services/pokedex.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [SearchBarComponent, NgIf],
+  imports: [SearchBarComponent, NgIf, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
