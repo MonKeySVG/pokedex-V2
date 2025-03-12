c63aa31a3e8fda5aca331f57979eee1e2629c761
Réda <37266352+MonKeySVG@users.noreply.github.com>
2025-03-12 11:17:38 +0100

    Correction de la liste des faiblesses et résistances pour correspondre aux valeurs réelles

--- src/app/services/pokedex.service.ts
+++ src/app/services/pokedex.service.ts
@@ -13,7 +13,11 @@ export class PokedexService {
 
   // A utiliser pour calculer les sensibilités d'un pokemon
   private typeEffectiveness: { [key: string]: { [key: string]: number } } = {
-    normal: { rock: 0.5, ghost: 0, steel: 0.5 },
+    normal: {
+      rock: 0.5,
+      ghost: 0,
+      steel: 0.5
+    },
     fire: {
       fire: 0.5,
       water: 0.5,
@@ -22,16 +26,23 @@ export class PokedexService {
       bug: 2,
       rock: 0.5,
       dragon: 0.5,
-      steel: 2,
+      steel: 2
     },
-    water: { fire: 2, water: 0.5, grass: 0.5, ground: 2, rock: 2, dragon: 0.5 },
+    water: {
+      fire: 2,
+      water: 0.5,
+      grass: 0.5,
+      ground: 2,
+      rock: 2,
+      dragon: 0.5
+    },
     electric: {
       water: 2,
       electric: 0.5,
       grass: 0.5,
       ground: 0,
       flying: 2,
-      dragon: 0.5,
+      dragon: 0.5
     },
     grass: {
       fire: 0.5,
@@ -43,7 +54,7 @@ export class PokedexService {
       bug: 0.5,
       rock: 2,
       dragon: 0.5,
-      steel: 0.5,
+      steel: 0.5
     },
     ice: {
       fire: 0.5,
@@ -53,7 +64,7 @@ export class PokedexService {
       ground: 2,
       flying: 2,
       dragon: 2,
-      steel: 0.5,
+      steel: 0.5
     },
     fighting: {
       normal: 2,
@@ -66,7 +77,7 @@ export class PokedexService {
       ghost: 0,
       dark: 2,
       steel: 2,
-      fairy: 0.5,
+      fairy: 0.5
     },
     poison: {
       grass: 2,
@@ -75,7 +86,7 @@ export class PokedexService {
       rock: 0.5,
       ghost: 0.5,
       steel: 0,
-      fairy: 2,
+      fairy: 2
     },
     ground: {
       fire: 2,
@@ -85,7 +96,7 @@ export class PokedexService {
       flying: 0,
       bug: 0.5,
       rock: 2,
-      steel: 2,
+      steel: 2
     },
     flying: {
       electric: 0.5,
@@ -93,9 +104,15 @@ export class PokedexService {
       fighting: 2,
       bug: 2,
       rock: 0.5,
-      steel: 0.5,
+      steel: 0.5
     },
-    psychic: { fighting: 2, poison: 2, psychic: 0.5, dark: 0, steel: 0.5 },
+    psychic: {
+      fighting: 2,
+      poison: 2,
+      psychic: 0.5,
+      dark: 0,
+      steel: 0.5
+    },
     bug: {
       fire: 0.5,
       grass: 2,
@@ -106,7 +123,7 @@ export class PokedexService {
       ghost: 0.5,
       dark: 2,
       steel: 0.5,
-      fairy: 0.5,
+      fairy: 0.5
     },
     rock: {
       fire: 2,
@@ -115,11 +132,26 @@ export class PokedexService {
       ground: 0.5,
       flying: 2,
       bug: 2,
-      steel: 0.5,
+      steel: 0.5
     },
-    ghost: { normal: 0, psychic: 2, ghost: 2, dark: 0.5 },
-    dragon: { dragon: 2, steel: 0.5, fairy: 0 },
-    dark: { fighting: 0.5, psychic: 2, ghost: 2, dark: 0.5, fairy: 0.5 },
+    ghost: {
+      normal: 0,
+      psychic: 2,
+      ghost: 2,
+      dark: 0.5
+    },
+    dragon: {
+      dragon: 2,
+      steel: 0.5,
+      fairy: 0
+    },
+    dark: {
+      fighting: 0.5,
+      psychic: 2,
+      ghost: 2,
+      dark: 0.5,
+      fairy: 0.5
+    },
     steel: {
       fire: 0.5,
       water: 0.5,
@@ -127,7 +159,7 @@ export class PokedexService {
       ice: 2,
       rock: 2,
       steel: 0.5,
-      fairy: 2,
+      fairy: 2
     },
     fairy: {
       fire: 0.5,
@@ -135,8 +167,8 @@ export class PokedexService {
       poison: 0.5,
       dragon: 2,
       dark: 2,
-      steel: 0.5,
-    },
+      steel: 0.5
+    }
   };
 
   constructor(private http: HttpClient) {
